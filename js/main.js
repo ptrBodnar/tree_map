303af206d6ec2656dea85c941f50e2c4aea70d37
ptr_bdr <peterbodnar1213@gmail.com>
2018-08-01 13:24:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ptr_bodnar on 16.07.18.
+ */
+var mymap = L.map('mapid').setView([48.51, 32.25], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.dark',
+    accessToken: 'pk.eyJ1IjoicHRyYmRyIiwiYSI6ImNqZG12dWdtYzBwdzgyeHAweDFueGZrYTYifQ.ZJ2tgs6E94t3wBwFOyRSBQ'
+}).addTo(mymap);
+
+
+d3.queue()
+    .defer(d3.csv, "data/cleaned_tree.csv")
+    .defer(d3.csv, "data/branch.csv")
+    .defer(d3.csv, "data/tree_planted.csv")
+    .await(function (err, data, branch, planted) {
+        if (err) throw err;
+        
+        createMap(data, branch, planted);
+        createBar(data);
+
+
+    })
+
---
+++ js/makeMap.js
@@ -0,0 +1,186 @@
+/**
+ * Created by ptr_bodnar on 26.07.18.
+ */
+
+function createMap(data, branch, planted) {
+    d3.selectAll(".leaflet-interactive").remove();
+    //d3.selectAll(".leaflet-control-layers-toggle").remove();
+
+    var color = d3.scaleLinear().domain([0, 10])
+        .range(['#70B5DC', '#0075B4']);
+
+
+    function nested_geojson(data) {
+
+        var nested = d3.nest()
+            .key(function (d) {
+                return "" + d.Latitude + "," + d.Longitude
+            })
+            .entries(data);
+
+
+        var geojson = nested.map(function (d) {
+            var a = d.key.split(",");
+            return {
+                type: "Feature",
+                properties: d,
+                geometry: {
+                    type: "Point",
+                    coordinates: [+a[1], +a[0]]
+                }
+            }
+        })
+
+        return geojson
+
+    }
+    
+
+    geojsonLayer = L.geoJson(nested_geojson(data), {
+        style: function (feature) {
+            var sumCut = 0;
+            feature.properties.values.forEach(function (d) {
+                sumCut += +d.was_cut;
+            })
+            return {color: color(sumCut)};
+        },
+        pointToLayer: function (feature, latlng) {
+            var sumNumber = 0;
+            feature.properties.values.forEach(function (d) {
+                sumNumber += +d.number
+            })
+
+            if (sumNumber < 3) {
+                sumNumber = 3;
+            }
+            if (sumNumber > 15) {
+                sumNumber = 15;
+            }
+            return new L.CircleMarker(latlng, {radius: sumNumber, fillOpacity: 0.75});
+        }
+    });
+
+    geojsonLayerBranch = L.geoJson(nested_geojson(branch), {
+        style: function (feature) {
+            var sumCut = 0;
+            feature.properties.values.forEach(function (d) {
+                sumCut += +d.was_cut;
+            })
+            return {color: color(sumCut)};
+        },
+        pointToLayer: function (feature, latlng) {
+            var sumNumber = 0;
+            feature.properties.values.forEach(function (d) {
+                sumNumber += +d.number
+            })
+
+            if (sumNumber < 3) {
+                sumNumber = 3;
+            }
+            if (sumNumber > 15) {
+                sumNumber = 15;
+            }
+            return new L.CircleMarker(latlng, {radius: sumNumber, fillOpacity: 0.75});
+        }
+    });
+
+    geojsonLayerPlanted = L.geoJson(nested_geojson(planted), {
+        style: {color: "green"},
+        pointToLayer: function (feature, latlng) {
+            var sumNumber = 0;
+            feature.properties.values.forEach(function (d) {
+                sumNumber += +d.number
+            })
+
+            if (sumNumber < 3) {
+                sumNumber = 3;
+            }
+            if (sumNumber > 15) {
+                sumNumber = 15;
+            }
+            return new L.CircleMarker(latlng, {radius: sumNumber, fillOpacity: 0.75});
+        }
+    });
+
+
+
+    mymap.addLayer(geojsonLayer);
+
+
+
+    overlayMaps = {
+        "Зрубування дерев": geojsonLayer,
+        "Обрізання дерев": geojsonLayerBranch,
+        "Висадження нових дерев": geojsonLayerPlanted
+    };
+
+    L.control.layers(overlayMaps).addTo(mymap);
+
+    geojsonLayer.on("click", function (event) {
+        debugger;
+        var streets = [];
+        var actNumber = [];
+
+        d3.select("div.mystyle").style("display", "flex")
+        d3.selectAll(".mystyle *").remove();
+
+        var totalTree = d3.sum(event.layer.feature.properties.values, function (d) {
+            return +d.number
+        });
+        var totalCut = d3.sum(event.layer.feature.properties.values, function (d) {
+            return +d.was_cut
+        });
+
+
+        d3.select(".mystyle").append("p").attr("class", "total").text("Загальна кількість дерев: "
+            + totalTree + " " + "Зрубано: " + totalCut);
+
+        var element = d3.select(".mystyle")
+            .selectAll(".element")
+            .data(event.layer.feature.properties.values)
+            .enter()
+            .append("div")
+            .attr("class", "element");
+
+
+        element.append("h4").text(function (d) {
+            if (!streets.includes(d.tree_adress_shorten)) {
+                streets.push(d.tree_adress_shorten)
+                return d.tree_adress_shorten
+            }
+        })
+
+
+        element.append("h5").text(function (d) {
+            if (!actNumber.includes(d.act_number)) {
+                actNumber.push(d.act_number)
+                return d.act_number
+            }
+        })
+
+        element.append("p").text(function (d) {
+            totalTree += +d.number;
+            totalCut += +d.was_cut;
+
+            return d.tree_characteristics + " Заплановано: " + d.number + " Вже зрубано: " + d.was_cut
+        })
+    })
+
+
+    mymap.on('baselayerchange', function (e) {
+        addPopUp(e);
+        if (e.name == 'Зрубування дерев') {
+            createBar(data);
+        }
+        if (e.name == 'Обрізання дерев') {
+            createBar(branch);
+        }
+        if (e.name == 'Висадження нових дерев') {
+            createBar(planted);
+        }
+
+    });
+
+
+}
+
