42cb9ace9e54961f2e04e4f2661ca303a523a568
ptr_Bodnar <peterbodnar1213@gmail.com>
2018-08-13 17:14:48 +0300

    made shorter

--- js/barFilter.js
+++ js/barFilter.js
@@ -24,7 +24,7 @@
     
 
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 400 - margin.left - margin.right,
+        width = 350 - margin.left - margin.right,
         height = 150 - margin.top - margin.bottom;
 
 // set the ranges
